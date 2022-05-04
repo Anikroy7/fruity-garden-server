efdd7ec28ca856a331101463cdf6ba18bc60f312
Anikroy7 <roy645800@gmail.com>
2022-05-04 18:58:52 +0600

    load product by conneting mongo db

--- index.js
+++ index.js
@@ -2,14 +2,50 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
+require('dotenv').config()
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
+// connetion to db
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wgy7w.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+
+    try {
+        await client.connect();
+
+        const productCollection = client.db('fruityGarden').collection('product');
+
+        app.get('/product', async (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products)
+        })
+
+    }
+    finally {
+        // client.close()
+    }
+}
+
+run().catch(console.dir)
+
+
 app.get('/', (req, res) => {
     res.send('fruity garden server is running')
 })
+
+// app.get('/product', (req, res) => {
+
+// })
+
 
 app.listen(port, () => {
     console.log('furity port is', port);
